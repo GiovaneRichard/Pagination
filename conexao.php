97c364b634a5f9dec434c3411cff2c378211a22d
Giovane Richard <giovanerichard@gmail.com>
2022-10-20 11:28:42 -0300

    Conexão com DB

--- conexao.php
+++ conexao.php
@@ -1,20 +1,7 @@
 <?php
 
-?>
+$db_type = "mysql:dbname=test;host=localhost";
+$db_user = "root";
+$db_password = "";
 
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
-    <title>DOM | Estudos</title>
-    <link rel="stylesheet" href="assets/css/style.css" />
-</head>
-<body>
-    
-    
-
-    <script src="assets/js/script.js"></script>
-</body>
-</html>
+$pdo = new PDO($db_type, $db_user, $db_password);
--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+    require 'conexao.php';
+
+    $item_per_pag = 10;
+    $pagina = 0;
+
+    $list = [];
+    $sql = $pdo->query("SELECT * FROM movies LIMIT $pagina, $item_per_pag");
+    $res = $sql->fetchAll(PDO::FETCH_ASSOC);
+   
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -8,8 +21,14 @@
     <link rel="stylesheet" href="assets/css/style.css" />
 </head>
 <body>
+
+    <h1>Lista de Filmes</h1>
     
-    
+    <ul>
+        <?php foreach($res as $item): ?>
+            <li><?= $item['title']?></li>
+        <?php endforeach ?>
+    </ul>
 
     <script src="assets/js/script.js"></script>
 </body>
